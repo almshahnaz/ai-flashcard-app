4b87e5263b2e1803738301530c10b3eeaf5a606c
almshahnaz <Shahnazalam115@gmail.com>
2026-01-05 10:15:12 -0500

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+        <section className="main-section">
+            <div className="page-heading">
+                <h1 className="text-3xl">Transform PDFs into Smart Flashcards</h1>
+                <h2>Upload any document and let AI instantly create personalized flashcards and study questions tailored to your learning style.</h2>
+            </div>
+        </section>
+    </main>
+}
